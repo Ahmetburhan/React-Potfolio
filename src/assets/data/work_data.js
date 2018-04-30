0f82574c375c69cbadb1a446fc09c0182da7cc3e
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-04-30 15:13:52 -0700

    updated projects added

--- src/assets/data/work_data.js
+++ src/assets/data/work_data.js
@@ -4,17 +4,58 @@ import burger from '../images/work/burger.png';
 import giphy from '../images/work/giphy.gif';
 import memorygame from '../images/work/memorygame.gif';
 import mongoose from '../images/work/mongoose.gif';
+import friend from '../images/work/friend.png';
+import train from '../images/work/train.png';
+import portfoliosimple from '../images/work/portfoliosimple.png';
+import petme from '../images/work/petme.png';
+import hangman from '../images/work/hangman.png';
+
+
+
+
+
 
 export default [
     {
         name: 'ParkHero',
-        info: 'ParkHero is an app that uses React technologies for dynamic front end interacting with an Express back end server and MongoDB for our database in order to provide a dynamic parking lot experience. For web scraping, the app uses the request from superagemt multi times in client/react side.',
-        img: parkheroGif,
+        info: 'ParkHero is an app that uses React technologies for dynamic front end interacting with an Express back end server and MongoDB for our database in order to provide a dynamic parking lot experience. For web scraping, the app uses the request from superagemt multi times in client/react side. ',
+        img: parkhero,
         alt: 'ParkHero',
         codeLink: "https://github.com/Ahmetburhan/ParkHero",
-        demoLink: "https://flexnotes.net/",
-        youTubeLink: "https://flexnotes.net/",
-        Giphy: parkhero
+        demoLink: "http://parkhero.herokuapp.com/",
+        youtube: "https://www.youtube.com/watch?v=UcKDdcHC44E&feature=youtu.be",
+    },
+    {
+        name: 'PetMe #LetsAdopt',
+        info: 'Pet Finder application built on jQuery, Ajax, and Bootstrap and MySql database.',
+        img: petme,
+        alt: 'PetMe #LetsAdopt',
+        codeLink: "https://github.com/Ahmetburhan/PetFinderME",
+        demoLink: "https://pet-finder-me.herokuapp.com/"
+    },
+    {
+        name: 'Match Game with React!',
+        info: 'Match Two! is a match game. This project was built using React, Bootstrap, scalable vector graphics (SVG), Photoshop, and Illustrator.',
+        img: memorygame,
+        alt: 'Match Game with React!',
+        codeLink: "https://github.com/Ahmetburhan/React-Memory-Game",
+        demoLink: "https://berkeley-match.herokuapp.com/"
+    },
+    {
+        name: 'MongoDB/Mongoose NYT web-scraper',
+        info: 'MongooseScraper This is an app that uses Cheerio to scrape the New York Times US section and pull the most recent top articles. the app uses express to serve routes and mongoose to interact with a MongoDB database.frontend, the app uses handlebars for templating each article and materialize as a styling framework.The app also uses jQuery and AJAX to help with making post requests. webscraping, the app uses the request and cheerio node packages. All webscrapping code can be found in the scrape.js file.',
+        img: mongoose,
+        alt: 'MongoDB/Mongoose NYT web-scraper',
+        codeLink: "https://github.com/Ahmetburhan/MongooseScraper",
+        demoLink: "https://mongo-scraper-cheerio.herokuapp.com/"
+    },
+    {
+        name: 'HangmanGame!',
+        info: 'Hangman Game with done w/CSS3 ,Bootstrap and some humor of Simpsons along with Giphys',
+        img: hangman,
+        alt: 'HangmanGame!',
+        codeLink: "https://github.com/Ahmetburhan/Hangman_Ahmet",
+        demoLink: "https://ahmetburhan.github.io/Hangman_Ahmet/"
     },
     {
         name: 'Burger Franchise',
@@ -33,20 +74,21 @@ export default [
         demoLink: "https://ahmetburhan.github.io/Giphy/"
     },
     {
-        name: 'Match Game with React!',
-        info: 'Match Two! is a match game. This project was built using React, Bootstrap, scalable vector graphics (SVG), Photoshop, and Illustrator.',
-        img: memorygame,
-        alt: 'Match Game with React!',
-        codeLink: "https://github.com/Ahmetburhan/React-Memory-Game",
-        demoLink: "https://berkeley-match.herokuapp.com/"
+        name: 'CalTrain Scheduler',
+        info: 'CalTrain Scheduler,built with materilize, canvas, modals, html5, css3.',
+        img: train,
+        alt: 'CalTrain Scheduler',
+        codeLink: "https://github.com/Ahmetburhan/CalTrainSchedule",
+        demoLink: "https://ahmetburhan.github.io/CalTrainSchedule/"
     },
     {
-        name: 'MongoDB/Mongoose NYT web-scraper',
-        info: 'MongooseScraper This is an app that uses Cheerio to scrape the New York Times US section and pull the most recent top articles. the app uses express to serve routes and mongoose to interact with a MongoDB database.frontend, the app uses handlebars for templating each article and materialize as a styling framework.The app also uses jQuery and AJAX to help with making post requests. webscraping, the app uses the request and cheerio node packages. All webscrapping code can be found in the scrape.js file.',
-        img: mongoose,
-        alt: 'MongoDB/Mongoose NYT web-scraper',
-        codeLink: "https://github.com/Ahmetburhan/MongooseScraper",
-        demoLink: "https://mongo-scraper-cheerio.herokuapp.com/"
+        name: 'friend finder 2.0',
+        info: 'ANSWER A FEW QUESTIONS TO FIND A FRIEND WITH THE GREATEST COMPATIBILITY, bootstrap, canvas, modals, html5, css3.',
+        img: friend,
+        alt: 'friend finder 2.0',
+        codeLink: "https://github.com/Ahmetburhan/FriendFinder2.0",
+        demoLink: "https://friend-finder-20.herokuapp.com/"
     },
+    
     
 ];
--- src/components/card.js
+++ src/components/card.js
@@ -5,15 +5,24 @@ export default props =>{
     if(props.demo !== ""){
         demoBtn = (<a className="waves-effect waves-light btn" target="_blank" href={props.demo}><i className="material-icons right">present_to_all</i> demo</a>);
     } 
+    let youtubeBtn = null;
+    if (props.youtube !== "") {
+        youtubeBtn = (<a className="waves-effect waves-light btn" target="_blank" href={props.youtube}><i className="material-icons right">video_library</i> youtube</a>);
+    } 
+
+    let giphyBtn = null;
+    if (props.giphy !== "") {
+        giphyBtn = (<a className="waves-effect waves-light btn" target="_blank" href={props.giphy}><i className="material-icons right">photo_library</i> giphy</a>);
+    } 
     return(
-    <div className="card col m6 s12 l4">
+    <div className="card col m6 s12">
         <div className="card-image waves-effect waves-block waves-light">
             <img className="activator work-img" src={props.src} alt={props.alt}/>
         </div>
         <div className="card-content">
             <span className="card-title activator">{props.title}<i className="material-icons right">add</i></span>
             <a className="waves-effect waves-light btn" target="_blank" href={props.code}><i className="material-icons right">developer_mode</i> code</a>
-            {demoBtn}        
+            {demoBtn}   {youtubeBtn}   {giphyBtn}   
         </div>
 
         <div className="card-reveal">
