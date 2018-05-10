3c5a0a12917f267c0317b21a55126f0297c88168
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-05-09 20:03:14 -0700

    cloud added

--- src/components/work.js
+++ src/components/work.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Card from './card';
 import workDataArray from '../assets/data/work_data';
-
+import WordCloud from '../assets/images/landing/wordcloud_ahmet.svg';
 
 export default props=>{
-    
+    // const WordCloud = require ('../assets/images/landing/wordcloud_ahmet.svg')
     const cardList = workDataArray.map((item, index) =>{
         return(
             <Card title={item.name} info={item.info} src={item.img} key={index} alt={item.alt} code={item.codeLink} demo={item.demoLink} giphy={item.giphy} youtube={item.youtube}/>
@@ -16,7 +16,9 @@ export default props=>{
 
             <hr/>
             <br />
-            <h4>Some of the Projects...</h4>
+            <h4 style={{ color: "#0cb5f0", fontSize: "2.2em"}}>Some of the Projects...</h4>
+            {/* <WordCloud /> */}
+            <img style={{ paddingLeft: "15%" }} src={WordCloud} alt="Wordcloud" />
             <div className="row">
             {cardList}
             </div>
